92aa4287f0e0ae2985919057db315173db01aa51
Dominik Foerderreuther <df@adobe.com>
2017-12-29 16:12:10 +0100

    jmx mbean dump util

--- src/main/java/com/adobe/acs/jmx2log/impl/Jmx2logConfigServlet.java
+++ src/main/java/com/adobe/acs/jmx2log/impl/Jmx2logConfigServlet.java
@@ -3,8 +3,11 @@ package com.adobe.acs.jmx2log.impl;
 import com.adobe.acs.jmx2log.ReadJmxService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.management.ObjectName;
+import javax.management.RuntimeMBeanException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -16,85 +19,55 @@ import java.io.PrintWriter;
 /**
  * Created by Dominik Foerderreuther <df@adobe.com> on 25.12.17.
  */
-@Component(service = Servlet.class, property = {"felix.webconsole.label=JMX2Log", "felix.webconsole.title=JMX to Log"})
+@Component(service = Servlet.class, property = {"felix.webconsole.label=JMX-Dump", "felix.webconsole.title=JMX mBean dump"})
 public class Jmx2logConfigServlet extends HttpServlet {
+
+    private final Logger log = LoggerFactory.getLogger(Jmx2logConfigServlet.class);
 
     @Reference
     ReadJmxService readJmxService;
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        //search(res.getWriter());
         complete(res.getWriter());
     }
 
-    private void search(PrintWriter writer) {
-        try {
-            final PrintWriter os = new PrintWriter(writer);
-            os.println("<table>");
+    private void complete(PrintWriter writer) {
 
-            // org.apache.jackrabbit.oak:name="/etc/replication//\*[11111b, no local]@com.day.cq.replication.impl.ConfigManagerImpl",type=BackgroundObserverStats,listenerId=9
-            String search = ".*replication.*type=agent.*id=\"publish\".*";
-            final String attributeNamePattern = "QueueNumEntries";
-            os.println("  <tr><td>search: </td><td colspan='3'>" + search + "</td></tr>");
-            os.println("  <tr><td>attributeNamePattern: </td><td colspan='3'>" + attributeNamePattern + "</td></tr>");
+        final PrintWriter os = new PrintWriter(writer);
 
-            for (final ObjectName mbean : readJmxService.mBeans(search)) {
-                os.println("  <tr><td colspan='4'>&nbsp;</td></tr>");
-                os.println("  <tr><td colspan='4'>" + mbean + "</td></tr>");
 
-                for (final ReadJmxService.MBeanAttribute attribute : readJmxService.value(mbean, attributeNamePattern)) {
-                    os.print("  <tr><td>&nbsp;</td><td>" + attribute.name() + "</td><td>" + attribute.type() + "</td><td>");
+        for (final ObjectName mbean : readJmxService.mBeans()) {
+            os.println("<table class=\"nicetable ui-widget\">\n" +
+                    "<thead class=\"ui-widget-header\">\n" +
+                    "<tr>\n" +
+                    "<th colspan=\"3\" class=\"ui-widget-header\">" + mbean + "</th>\n" +
+                    "</tr>\n" +
+                    "</thead>\n");
 
-                    final Object o = attribute.value();
-                    if (o == null) {
-                        os.print("<font>null</font>");
-                    } else {
-                        os.print(o.toString());
-                    }
+            os.println("<tbody class=\"ui-widget-content\">\n");
 
-                    os.println("</td></tr>");
-                }
-            }
-            os.println("</table>");
-            os.flush();
-
-        } catch (ReadJmxService.CouldNotReadJmxValueException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void complete(PrintWriter writer) throws IOException {
-        try {
-
-            final PrintWriter os = new PrintWriter(writer);
-            os.println("<table>");
-
-            for (final ObjectName mbean : readJmxService.mBeans()) {
-                os.println("  <tr><td colspan='4'>&nbsp;</td></tr>");
-                os.println("  <tr><td colspan='4'>" + mbean + "</td></tr>");
-
+            try {
                 for (final ReadJmxService.MBeanAttribute attribute : readJmxService.value(mbean)) {
-                    os.print("  <tr><td>&nbsp;</td><td>" + attribute.name()
-                            + "</td><td>" + attribute.type() + "</td><td>");
 
-                    final Object o = attribute.value();
-                    if (o == null) {
-                        os.print("<font color='#660000'>null</font>");
-                    } else {
-                        os.print(o.toString());
-                    }
+                    os.println("<tr>\n" +
+                            "<td style=\"width:20%\">" + attribute.name() + "</td>\n" +
+                            "<td style=\"width:20%\">" + attribute.type() + "</td>\n" +
+                            "<td>" + attribute.value() + "</td>\n" +
+                            "</tr>\n");
 
-                    os.println("</td></tr>");
                 }
+
+            } catch (RuntimeMBeanException | ReadJmxService.CouldNotReadJmxValueException e) {
+                log.error("cant read attributes of " + mbean.toString(), e);
             }
 
-            os.println("</table>");
-            os.flush();
-
-        } catch (ReadJmxService.CouldNotReadJmxValueException e) {
-            e.printStackTrace();
+            os.println("</tbody>\n");
+            os.println("</table>\n");
         }
+
+        os.flush();
+
     }
 
 }
