3e5706f23760d1c7db3f40114ef37567c081cf7f
Dominik Foerderreuther <df@adobe.com>
2017-12-29 13:04:39 +0100

    config list

--- bundle/src/main/java/com/adobe/acs/jmx2log/JmxToLogService.java
+++ bundle/src/main/java/com/adobe/acs/jmx2log/JmxToLogService.java
@@ -1,9 +1,11 @@
 package com.adobe.acs.jmx2log;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.PropertyUnbounded;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.osgi.PropertiesUtil;
@@ -12,6 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.management.ObjectName;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Dominik Foerderreuther <df@adobe.com> on 25.12.17.
@@ -26,28 +30,39 @@ public class JmxToLogService implements Runnable {
 
     protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private static final String DEFAULT_CANONICAL_NAME_PATTERN = ".*replication.*type=agent.*id=\"publish\".*";
-    @Property(description="Regex Pattern of canonical name", value=DEFAULT_CANONICAL_NAME_PATTERN)
-    private static final String CANONICAL_NAME_PATTERN = "jmxtolog.canonical.pattern";
-    private String canonicalNamePattern;
+    private static final String DEFAULT_SEARCH_CONFIG = ".*replication.*type=agent.*id=\"publish\".*|QueueNumEntries";
+    @Property(unbounded = PropertyUnbounded.ARRAY, cardinality=10, label="Search", description="Regex Pattern for jmx-bean and attribute in format beanpattern|attributpattern", value=DEFAULT_SEARCH_CONFIG)
+    private static final String SEARCH_CONFIG = "jmxtolog.search.configs";
 
-    private static final String DEFAULT_ATTRIBUTE_NAME_PATTERN = "QueueNumEntries";
-    @Property(description="Regex Pattern of attribute name", value=DEFAULT_ATTRIBUTE_NAME_PATTERN)
-    private static final String ATTRIBUTE_NAME_PATTERN = "jmxtolog.attribute.pattern";
-    private String attributeNamePattern;
+    private List<SearchConfig> searchConfigs;
 
     @Reference
     ReadJmxService readJmxService;
 
     @Activate
     protected void activate(ComponentContext ctx) {
-        canonicalNamePattern = PropertiesUtil.toString(ctx.getProperties().get(CANONICAL_NAME_PATTERN), DEFAULT_CANONICAL_NAME_PATTERN);
-        attributeNamePattern = PropertiesUtil.toString(ctx.getProperties().get(ATTRIBUTE_NAME_PATTERN), DEFAULT_ATTRIBUTE_NAME_PATTERN);
+        searchConfigs = new ArrayList<>();
+        String[] strSearchConfigs = PropertiesUtil.toStringArray(ctx.getProperties().get(SEARCH_CONFIG), new String[]{DEFAULT_SEARCH_CONFIG});
+        for (String strSearchConfig : strSearchConfigs) {
+            if (StringUtils.isEmpty(strSearchConfig)) {
+                continue;
+            }
+            String[] parts = strSearchConfig.split("|");
+            String namePattern = parts[0];
+            String attributePattern = parts.length > 0 ? parts[1] : "";
+            searchConfigs.add(new SearchConfig(namePattern, attributePattern));
+        }
     }
 
     public void run() {
         log.trace("run");
-        for (ObjectName mBean : readJmxService.mBeans(canonicalNamePattern)) {
+        for (SearchConfig searchConfig : searchConfigs) {
+            logJmxValues(searchConfig.getNamePattern(), searchConfig.getAttributePattern());
+        }
+    }
+
+    private void logJmxValues(String namePattern, String attributeNamePattern) {
+        for (ObjectName mBean : readJmxService.mBeans(namePattern)) {
             try {
                 for (ReadJmxService.MBeanAttribute mBeanAttribute : readJmxService.value(mBean, attributeNamePattern)) {
                     log.trace(mBeanAttribute.name() + ": " + mBeanAttribute.value());
@@ -55,6 +70,26 @@ public class JmxToLogService implements Runnable {
             } catch (ReadJmxService.CouldNotReadJmxValueException e) {
                 log.error(String.format("cant read mBean values for %s", mBean.toString()), e);
             }
+        }
+    }
+
+    private static class SearchConfig {
+
+        private final String namePattern;
+
+        private final String attributePattern;
+
+        SearchConfig(String canonicalNamePattern, String attributeNamePattern) {
+            this.namePattern = canonicalNamePattern;
+            this.attributePattern = attributeNamePattern;
+        }
+
+        String getNamePattern() {
+            return namePattern;
+        }
+
+        String getAttributePattern() {
+            return attributePattern;
         }
     }
 }
